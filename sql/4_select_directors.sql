8e55b334aedcad3af8dc3a7041c539746b01aa45
hustle2live <kuz.v@ukr.net>
2024-07-27 08:20:52 +0300

    feat: + added fourth query

--- sql/4_select_directors.sql
+++ sql/4_select_directors.sql
@@ -1,5 +1,24 @@
 -- Вибрати режисерів разом із середнім бюджетом фільмів, які вони зняли
 -- Shape: -- Director ID -- Director name (concatenation of first and last names) -- Average budget
 
+-- add some direktors to persons
 
+ INSERT INTO person (first_name, last_name, birth_date, country_id, gender) VALUES
+ ('Haris', 'Peterson', '1999-11-08', 1, 'male'),
+ ('Marlon', 'Baker', '1974-01-11', 1, 'male');
 
+--  select directors with budgets
+
+SELECT p.id, concat(p.first_name, ' ', p.last_name) AS director_name, AVG(CAST(m.budget AS NUMERIC)) AS avg_budget
+FROM person p
+RIGHT JOIN movie m ON m.producer ILIKE concat('%', p.first_name, ' ', p.last_name, '%')
+GROUP BY p.id, director_name;
+
+-- result
+
+--  id | director_name  |      avg_budget
+-- ----+----------------+-----------------------
+--   1 | William Worth  | 72701200.000000000000
+--   3 | James White    | 84962400.000000000000
+--  14 | Haris Peterson | 52272350.000000000000
+--  15 | Marlon Baker   | 18554933.333333333333
